dcca0cd902a7f760b32991ea820eb6684693a46b
Gera <gr.misterg@gmail.com>
2024-08-28 13:04:49 -0600

    dummy comment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ siiaPath = r'D:\omarg\Descargas\carga siia 232.xlsx' # ! Change for user path
 chPath = r'D:\omarg\Descargas\CH 2023-2.xlsx' # ! Change for user path
 
 siia = pd.read_excel(siiaPath, usecols="C:F,H,K,N,R:V,Z,AJ,AL,AN,AP,AR")
-ch = pd.read_excel(chPath,skiprows = 4)
+ch = pd.read_excel(chPath,skiprows = 4) #bien
 
 # * DATA CLEANING PROCESS
 
