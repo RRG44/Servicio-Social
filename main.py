e3d71b3280721ef7a96c2f142da82a956d8896f5
Omsod0404 <garciaomarucho@gmail.com>
2024-08-28 10:33:46 -0600

    Colums used in dataframes

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 from utilities import separateHours 
 
-siia = pd.read_excel(r'carga siia 232.xlsx') # ! Change for user path
-ch = pd.read_excel(r'CH 2023-2.xlsx', skiprows = 4) # ! Change for user path
+siiaPath = r'D:\omarg\Descargas\carga siia 232.xlsx' # ! Change for user path
+chPath = r'D:\omarg\Descargas\CH 2023-2.xlsx' # ! Change for user path
+
+siia = pd.read_excel(siiaPath, usecols="C:F,H,K,N,R:V,Z,AJ,AL,AN,AP,AR")
+ch = pd.read_excel(chPath,skiprows = 4)
 
 # * DATA CLEANING PROCESS
 
