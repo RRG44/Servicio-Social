54906bb991b76cf03c0056c84bd1b63bcc11c497
JD1975 <93783584+JD1975@users.noreply.github.com>
2024-08-28 15:12:24 -0600

    Add new function (Remove accents) & FORMATTING TEXT PROCESS

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
-from utilities import separateHours 
+import unicodedata
+import re
+from utilities import separateHours, remove_accents
 
 siiaPath = r'cargasiia232.xlsx' # ! Change for user path
 chPath = r'CH2023-2.xlsx' # ! Change for user path
 
+#Importing the siia EXCEL file with the columns that we only need (Check the Google DOCS), and with MAESTRO column as float
 siia = pd.read_excel(siiaPath, usecols="C:F,H,K,N,R:V,Z,AJ,AL,AN,AP,AR").astype({"MAESTRO":'float'})
+#Importing the CH EXCEL, skiping the image on it
 ch = pd.read_excel(chPath,skiprows = 4) #bien
 
 # * DATA CLEANING PROCESS
@@ -16,5 +20,19 @@ for d in dias:
     siia[[d[:2] , d[:2]+'.1']] = siia[d].apply(separateHours).apply(pd.Series)
 for d in dias:
     siia.drop(d, axis = 1)
+    
+# * FORMATTING TEXT PROCESS
+
+# Aply the funcion to eliminate acents and replace the dots in the columns
+siia['NOMBRE'] = siia['NOMBRE'].apply(remove_accents).str.replace(r'[.,]', '', regex=True)
+siia['NOMBREMATE'] = siia['NOMBREMATE'].apply(remove_accents).str.replace(r'[.,]', '', regex=True)
+
+# Replace the - signs in the NOMBRE column (Professor's name) with the Ñ
+siia['NOMBRE'] = siia['NOMBRE'].str.replace(r'—', 'Ñ', regex=True)
+# Replace the - signs in the NOMBREMATE column (Subject's name) with the Ñ
+siia['NOMBREMATE'] = siia['NOMBREMATE'].str.replace("—", "Ñ", case=False, regex=True)
+
+# Modifies the 'GRUPO' column, so it matches the 'GRUPO' column from the FIF
+siia['GRUPO'] = siia['GRUPO'] % 100
 
 # * COMPARE PROCESS
--- utilities.py
+++ utilities.py
@@ -1,4 +1,4 @@
-
+# Function to separate the hours of the string hh:mm-hh:mm
 def separateHours(hours):
     """This function transforms string hh:mm-hh:mm to float
         In: string Out: float, float"""
@@ -12,3 +12,10 @@ def separateHours(hours):
             split[i] = split[i][1:]
 
     return float(split[0]), float(split[1]) # Example hours: 8:00-9:00, output: 8.0, 9.0
+
+# Function to eliminate the acents and signs of punctuation in the data frame
+def remove_accents(input_str):
+    if isinstance(input_str, str):  # Verifica si el valor es una cadena
+        nfkd_form = unicodedata.normalize('NFKD', input_str)
+        return ''.join([c for c in nfkd_form if not unicodedata.combining(c)])
+    return input_str  # Devuelve el valor tal cual si no es una cadena
