503cb8c99b8b0f444c9f7266b95cfb60f4330341
Gera <gr.misterg@gmail.com>
2024-09-04 13:57:37 -0600

    [feature/fix]: function highlight added and implemented in main. Fixed bug caused by Angel's commit

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ import utilities as util
 siiaPath = r'cargasiia232.xlsx' # ! Change for user path
 chPath = r'CH2023-2.xlsx' # ! Change for user path
 
+# file reading
 siia = util.read_siia(siiaPath)
 ch = util.read_ch(chPath)
+
+# highlighting
+dfp = util.highlight_differences(siia, ch)
+dfp.to_excel('comparison_output.xlsx', engine='openpyxl', index=False)
