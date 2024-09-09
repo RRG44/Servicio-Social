df821cf71e8520b868c28d5e1d056f0172c26726
Luis Angel Reyes Ventura <angelfeik2.0@gmail.com>
2024-09-09 13:44:51 -0600

    adding black divisor

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 import utilities as util
 
-siiaPath = r'carga siia 232.xlsx' # ! Change for user path
-chPath = r'CH 2023-2.xlsx' # ! Change for user path
+siiaPath = r'cargasiia232.xlsx' # ! Change for user path
+chPath = r'CH2023-2.xlsx' # ! Change for user path
 
 # file reading
 siia = util.read_siia(siiaPath)
 ch = util.read_ch(chPath)
 siia = util.change_col_order(siia)
+siia.insert(0, "div", "")
 
 # highlighting
 dfp = util.highlight_differences(siia, ch)
@@ -16,18 +17,25 @@ dfp.data = util.insert_na(dfp.data)
 
 dfp.set_properties(**{
         'border': '1px solid black',
-        'text-align': 'center'
+        'text-align': 'center',
     })
 
 writer = pd.ExcelWriter('Comparasion.xlsx', engine='xlsxwriter')
-
 dfp.to_excel(writer, sheet_name='Sheet1', index=False)
 
 workbook = writer.book
 worksheet = writer.sheets['Sheet1']
+
+format_black = workbook.add_format({'bg_color': '#000000'})
+
+for row in range(1, len(dfp.data) + 1):  # Comienza en la fila 1
+    worksheet.write_blank(row, dfp.data.columns.get_loc('div'), None, format_black)  # Columna 1 corresponde a "Div"
 
 for i, col in enumerate(dfp.data.columns):
     max_len = max(dfp.data[col].apply(lambda x: len(str(x))).max(), len(col))
     worksheet.set_column(i, i, max_len + 1)
 
 writer.close()    
+
+
+
